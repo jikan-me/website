819a8b7b7d07a12b947c621c3eca62f48afe57c6
John Dinh <johnduydinh@gmail.com>
2019-10-08 18:07:39 -0400

    show and hide links on mobile when toggled

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,3 +1,23 @@
-document.getElementById('nav_trigger').onclick = () => {
-    document.getElementById('menu_mobile').classList.toggle('show');
-};
+// document.getElementById('nav_trigger').onclick = () => {
+//     document.getElementById('menu_mobile').classList.toggle('show');
+// };
+
+function triggerMenu( links, state){
+    if (state) {
+        links.style.opacity = 1;
+        links.style.zIndex = 1;
+    } else {
+        links.style.opacity = 0;
+        links.style.zIndex = -1;
+    }
+
+}
+
+const hamburgerIcon = document.querySelector(".menu-icon");
+const mobileLinks = document.querySelector(".menu__mobile-menu");
+let state = true;
+
+hamburgerIcon.addEventListener("click", () => {
+    triggerMenu(mobileLinks, state)
+    state = !state;
+});
