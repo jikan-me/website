d4183bbe940edc5020d803d39acee1270b55ed98
Irfan <irfan-dahir@users.noreply.github.com>
2024-09-19 20:59:17 +0500

    update google analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,3 @@
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-S3KKT32CL5"></script>
-<script>
-	window.dataLayer = window.dataLayer || [];
-	function gtag(){dataLayer.push(arguments);}
-	gtag('js', new Date());
-
-	gtag('config', 'G-S3KKT32CL5');
-</script>
 <div class="app">
 <!--	<Header />-->
 
