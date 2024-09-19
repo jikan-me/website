20073b665f5c56e743cbd37eb92abcbed0faa51a
Irfan <irfan-dahir@users.noreply.github.com>
2024-09-19 21:02:35 +0500

    remove google analytics from code

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,8 +1,3 @@
 export const ssr = false;
 export const prerender = true;
 // export const trailingSlash = 'always';
-
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-gtag('config', 'G-S3KKT32CL5');
