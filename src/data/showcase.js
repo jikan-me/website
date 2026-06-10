a74929111ace7bca7db913fbb9d0f673a1f715df
gorolmartin <goroldesign@gmail.com>
2026-06-10 23:01:57 +0200

    Update showcase.js
    Added AniSwipe app

--- src/data/showcase.js
+++ src/data/showcase.js
@@ -587,4 +587,10 @@ export default [
       imageUrl: "https://sorai-app.vercel.app/images/preview.png",
       url: "https://sorai-app.vercel.app/"
     },
+	{
+      title: "AniSwipe",
+      summary: "Swipe through seasonal anime and build your watchlist in minutes.",
+      imageUrl: "https://aniswipe.gethako.app/ogimage.png",
+      url: "https://aniswipe.gethako.app/"
+    },
 ]
